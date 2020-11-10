4d07852b1a9197dff60f3c652e91c1d6108b326d
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-10 14:20:53 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,86 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+# # require 'pry'
+
+# # # birthday_kids = {
+# # #   "Timmy" => 9, 
+# # #   "Sarah" => 6, 
+# # #   "Amanda" => 27
+# # # }
+
+# # # def happy_birthday(birthday_kids)
+# # #   birthday_kids.each do |key, value|
+# # #     puts "Happy Birthday #{key}! You are now #{value} years old!"
+# # #   end
+# # # end
+
+
+
+# # # def say_hi
+# # #   puts 'hi'
+# # #   binding.pry
+# # # end
+# # # def say_bye
+# # #   puts 'bye'
+# # #   binding.pry
+# # # end
+
+
+
+
+
+
+
+
+
+# # # def select_winner(hash)
+# # #   winner = ""
+# # #   hash.each do |room, name|
+# # #     if ()
+# # #   end
+# # # end
+
+
+
+
+# def happy_birthday(hash)
+#   hash.each do |name, age|
+#     puts "Happy Birthday #{name}! You are now #{age} years old!"
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |key, value|
-    puts "Happy Birthday #{key}! You are now #{value} years old!"
+  birthday_kids.each do |kid_name, age|
+    puts "Happy Birthday #{kid_name}! You are now #{age} years old!"
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,17 +1,91 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+# require 'pry'
+# # # passengers = {
+# # # suite_a: "Amanda Presley", 
+# # # suite_b: "Seymour Hoffman", 
+# # # suite_c: "Alfred Tennyson", 
+# # # suite_d: "Charlie Chaplin", 
+# # # suite_e: "Crumpet the Elf"
+# # # }
 
-def select_winner(passengers)
-  winner = ''
-  passengers.each do |key, value|
-    if key == :suite_a && value.start_with?("A")
-      winner += value
-    end
+# def select_winner(passengers)
+#   winner = ''
+#   passengers.each do |key, value|
+#     if key == :suite_a && value.start_with?("A")
+#       winner += value
+#     end
+#     binding.pry
+#   end
+#   winner
+# end
+# def say_hi
+#   puts 'hi'
+#   binding.pry
+# end
+# def say_bye
+#   puts 'bye'
+#   binding.pry
+#   puts 'hello'
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # def select_winner(passengers)
+# #   winner = ""
+# #   passengers.each do |key, value|
+# #     if key == :suite_a && value.start_with?("A")
+  
+# #       winner += value
+      
+# #     end
+# #   end
+# #   winner
+# # end
+
+
+
+require 'pry'
+
+
+
+def select_winner(hash)
+  winner = ""
+  hash.each do |room, name|
+    return winner << name if room == :suite_a && name[0] == "A"
   end
-  winner
+  #winner
 end
+
+
+
+passengers = {
+suite_a: "Amanda Presley", 
+suite_b: "Seymour Hoffman", 
+suite_c: "Alfred Tennyson", 
+suite_d: "Charlie Chaplin", 
+suite_e: "Crumpet the Elf"
+}
+puts select_winner(passengers)
+
+
+
+
+
+
+
+
+
+
+
+
