c077dce959baf7537ad82293b539f8bad704bbd1
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-10 14:20:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -64,7 +64,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kid_name, age|
-    puts "Happy Birthday #{kid_name}! You are #{age}!"
+    puts "Happy Birthday #{kid_name}! You are #{age} years old!"
   end
 end
 
