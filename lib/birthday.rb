68e04e95329d58a749d922bb41fb0c4e1e709a50
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-10 14:17:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,60 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+# require 'pry'
 
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |key, value|
-    puts "Happy Birthday #{key}! You are now #{value} years old!"
+# # birthday_kids = {
+# #   "Timmy" => 9, 
+# #   "Sarah" => 6, 
+# #   "Amanda" => 27
+# # }
+
+# # def happy_birthday(birthday_kids)
+# #   birthday_kids.each do |key, value|
+# #     puts "Happy Birthday #{key}! You are now #{value} years old!"
+# #   end
+# # end
+
+
+
+# # def say_hi
+# #   puts 'hi'
+# #   binding.pry
+# # end
+# # def say_bye
+# #   puts 'bye'
+# #   binding.pry
+# # end
+
+
+
+
+
+
+
+
+
+# # def select_winner(hash)
+# #   winner = ""
+# #   hash.each do |room, name|
+# #     if ()
+# #   end
+# # end
+
+
+
+
+def happy_birthday(hash)
+  hash.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
