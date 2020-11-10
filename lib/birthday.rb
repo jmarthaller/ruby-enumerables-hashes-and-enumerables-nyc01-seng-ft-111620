e7c3b917c74b5c3a3f0f7b78c08ce02ab49911c8
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-10 14:18:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,29 +1,27 @@
-# require 'pry'
+# # require 'pry'
 
-# # birthday_kids = {
-# #   "Timmy" => 9, 
-# #   "Sarah" => 6, 
-# #   "Amanda" => 27
-# # }
+# # # birthday_kids = {
+# # #   "Timmy" => 9, 
+# # #   "Sarah" => 6, 
+# # #   "Amanda" => 27
+# # # }
 
-# # def happy_birthday(birthday_kids)
-# #   birthday_kids.each do |key, value|
-# #     puts "Happy Birthday #{key}! You are now #{value} years old!"
-# #   end
-# # end
-
-
-
-# # def say_hi
-# #   puts 'hi'
-# #   binding.pry
-# # end
-# # def say_bye
-# #   puts 'bye'
-# #   binding.pry
-# # end
+# # # def happy_birthday(birthday_kids)
+# # #   birthday_kids.each do |key, value|
+# # #     puts "Happy Birthday #{key}! You are now #{value} years old!"
+# # #   end
+# # # end
 
 
+
+# # # def say_hi
+# # #   puts 'hi'
+# # #   binding.pry
+# # # end
+# # # def say_bye
+# # #   puts 'bye'
+# # #   binding.pry
+# # # end
 
 
 
@@ -31,21 +29,23 @@
 
 
 
-# # def select_winner(hash)
-# #   winner = ""
-# #   hash.each do |room, name|
-# #     if ()
-# #   end
-# # end
+
+
+# # # def select_winner(hash)
+# # #   winner = ""
+# # #   hash.each do |room, name|
+# # #     if ()
+# # #   end
+# # # end
 
 
 
 
-def happy_birthday(hash)
-  hash.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
-  end
-end
+# def happy_birthday(hash)
+#   hash.each do |name, age|
+#     puts "Happy Birthday #{name}! You are now #{age} years old!"
+#   end
+# end
 
 
 
